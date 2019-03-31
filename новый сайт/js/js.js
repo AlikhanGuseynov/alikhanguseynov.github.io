1267a686579790bf356b664eef029045dd5ccdd4
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-03-31 19:14:43 +0400

    new

--- новый сайт/js/js.js
+++ новый сайт/js/js.js
@@ -1,14 +1,11 @@
+
+// PAGINATION
+
 var page="";
 var home_top = $(".home").offset().top;
-// console.log(home_top);
 var about_top = $(".about").offset().top;
-// console.log(about_top);
-var projrct_top = $(".project").offset().top;
-// console.log(projrct_top);
-var contact_top = $(".contact").offset().top;
-// console.log(contact_top);
-
-
+// var project_top = $(".project").offset().top;
+// var contact_top = $(".contact").offset().top;
 $(window).scroll(function() {
 	var scrollTop = $(this).scrollTop();
 console.log( home_top );
@@ -21,16 +18,66 @@ if ( scrollTop > about_top-about_top/2) {
 	page = "2";
 	document.getElementById("page").innerHTML=page;
 }
-if ( scrollTop > projrct_top-about_top/2) {
-	page = 3;
-	document.getElementById("page").innerHTML=page;
-}
-if ( scrollTop > contact_top-about_top/2) {
-	page = 4;
-	document.getElementById("page").innerHTML=page;
-} 
+// if ( scrollTop > project_top-about_top/2) {
+// 	page = 3;
+// 	document.getElementById("page").innerHTML=page;
+// }
+// if ( scrollTop > contact_top-about_top/2) {
+// 	page = 4;
+// 	document.getElementById("page").innerHTML=page;
+// } 
+});
+
+
+// PETAL BLOCK
+$(".petal_information").click(function() {
+	 $( ".text_information" ).css( "display", "flex" );
+	 $( ".text_knowledge" ).css( "display", "none" );
+ 	 $( ".text_resume" ).css( "display", "none" );
+ 	 $( ".petal_information" ).css( "border-right", "none" );
+ 	 // $( ".petal_knowledge" ).css( "border-right", "2px solid #96A6C0" );
+ 	 // $( ".petal_resume" ).css( "border-right", "2px solid #96A6C0" );
+	 $( ".text_information" ).addClass( "animated fadeIn " );
 
 });
+$(".petal_knowledge").click(function() {
+	 $( ".text_information" ).css( "display", "none" );
+	 $( ".text_knowledge" ).css( "display", "flex" );
+ 	 $( ".text_resume" ).css( "display", "none" );
+ 	 // $( ".petal_information" ).css( "border-right", "2px solid #96A6C0" );
+ 	 $( ".petal_knowledge" ).css( "border-right", "none" );
+ 	 // $( ".petal_resume" ).css( "border-right", "2px solid #96A6C0" );
+	 $( ".text_knowledge" ).addClass( "animated fadeIn " );
+
+
+});
+$(".petal_resume").click(function() {
+	 $( ".text_information" ).css( "display", "none" );
+	 $( ".text_knowledge" ).css( "display", "none" );
+ 	 $( ".text_resume" ).css( "display", "flex" );
+ 	 // $( ".petal_information" ).css( "border-right", "2px solid #96A6C0" );
+ 	 // $( ".petal_knowledge" ).css( "border-right", "2px solid #96A6C0" );
+ 	 $( ".petal_resume" ).css( "border-right", "none" );
+	 $( ".text_resume" ).addClass( "animated fadeIn " );
+ 	 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
