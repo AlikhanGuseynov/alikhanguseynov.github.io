9c76c4fce0df5dbca205ef5166468d30d2b558ea
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-04-22 22:38:39 +0400

    new 22.04.19

--- js/matches.js
+++ js/matches.js
@@ -1,23 +1,13 @@
-var mql = window.matchMedia('all and (min-width: 1000px)');
+var mql = window.matchMedia('all and (max-width: 768px)');
 if (mql.matches) {
 
-	//	ANIMATE HOME	
 
-	$('.about_title').addClass('wow flipInX');
-	$('.first').addClass('wow bounceInLeft');
-	$('.second').addClass('wow bounceIn');
-	$('.thert').addClass('wow bounceInRight');
-	$('.progect_title').addClass('wow flipInX');
-	$('.pr_block').addClass('wow fadeIn');
-	$('.contact_title p').addClass('wow flipInX');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
-	$().addClass('wow');
+
+	//	ANIMATE burger_menu
+
+	
+
+
 
 
 }
--- portfolio/library/matches.js
+++
@@ -1,6 +0,0 @@
-var mql = window.matchMedia('all and (min-width: 1000px)');
-if (mql.matches) {
-
-	//	ANIMATE HOME	
-
-}
