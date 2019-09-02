91205b96819146e3e2301499e2ea03692888de4d
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-09-03 01:59:06 +0400

    balcony - вроде 5-ый заход

--- project/balcony/js/js.js
+++ project/balcony/js/js.js
@@ -1,5 +1,4 @@
-//////////////////////////////////
-////////////////////////// SLICK
+////////////////// SLICK
 $(document).ready(function(){
 $('.years_slider .slider-for').slick({ 
   slidesToShow: 1,
@@ -20,18 +19,14 @@ $('.years_slider .slider-nav').slick({
   nextArrow: '<button class="slick-next" aria-label="Next" type="button" onclick="slide_next()"> </button>',
 });
 });
-
-///////////////////////////////////
-//////////////////////// вывод колличества слайдов
+/////////////// вывод колличества слайдов
 var slides_amount = $(".years_slider .slider_nav div").length;
 $(".years_slider p.slides_amount").html("/" + slides_amount);
 
-///////////////////////////////////
 //////////////////////// вывод номера слайда
 var slide_number = 1;
 $(".years_slider p.slide_number").html(slide_number);
 
-///////////////////////////////////
 //////////////////////// кнопка next
 function slide_next(){
 if (slide_number < slides_amount ) {
@@ -42,7 +37,6 @@ slide_number++;
 $(".years_slider p.slide_number").html(slide_number);
 };
 
-///////////////////////////////////
 //////////////////////// кнопка prev
 function slide_prev(){
 if (slide_number > 1 ) {
@@ -52,17 +46,104 @@ slide_number--;
 }
 $(".years_slider p.slide_number").html(slide_number);
 }
-////////////////
+//////////////// второй слайдер
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 2,
   slidesToScroll: 1,
 });
+//////////////// всплывающий слайдер
+$('.up_slider_drag').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slick-prev up_slider_btn up_slider_btn_prev" aria-label="Previous" type="button""> </button>',
+  nextArrow: '<button class="slick-next up_slider_btn up_slider_btn_next" aria-label="Next" type="button""> </button>',
+  responsive: [
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 440,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    ]
+});
+
+
+
+
+
+$(".div_item li").click(function(event) {
+ var btn_class = $(this).attr('class');
+ $(".up_slider_box").css({
+   display: 'block',
+ });
+$("." + btn_class + "_block").addClass('display_flex')
+});
+
+
+
+$(".years_item_btn_2, .years_item_btn_5").click(function(event) {
+   $(".up_slider_box ").css({
+   // height: '450px',
+   color: 'red',
+ });
+});
+
+
+
+
+
+$("button.close_slider").click(function(event) {
+  $(".up_slider_box .display_flex").removeClass(' ');
+  $('.up_slider_box').css({
+    display: 'none',
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // кнопка заказа звонка
-
 $(".btn").click(function(event) {
   $(".block_form").css({
     display: 'flex',
