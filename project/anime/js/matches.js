0a73f697bf7ba8b9373ded8590e22b5f2f64c4b4
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-09-28 23:03:05 +0400

    anime_first

---
+++ project/anime/js/matches.js
@@ -0,0 +1,16 @@
+var mql = window.matchMedia('all and (max-width: 540px)');
+if (mql.matches) {
+
+
+$("ul.nav_catalog").click(function(event) {
+	$("ul.nav_catalog").animate({
+    height: "240px",
+  }, 1000);
+});
+
+	
+
+
+
+
+}
