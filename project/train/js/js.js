bc9a4d7674c2b53149dabb7857d81a9268aee175
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-11-14 00:25:11 +0400

    medai

--- project/train/js/js.js
+++ project/train/js/js.js
@@ -3,4 +3,23 @@ $('.multiple-items').slick({
   dots: true,
   slidesToShow: 2,
   slidesToScroll: 2,
+    responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
