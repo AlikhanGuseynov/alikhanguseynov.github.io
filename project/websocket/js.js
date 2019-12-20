5060f1b0c4a4d90ac5e4a2dc0fc8529ebf46fe6a
alikhanguseynov <alikhan-gusejjnv@list.ru>
2019-12-20 07:35:49 +0400

    websocket 1.1

--- project/websocket/js.js
+++ project/websocket/js.js
@@ -1,3 +1,6 @@
+let a = 0;
+let arr = [{ text: 'Lorem ipsum.', id: 0 }]
+
 var wsUri = "wss://echo.websocket.org/";
 var output;
 function init(){
@@ -13,20 +16,25 @@ function testWebSocket(){
 }
 function onOpen(evt){
   writeToScreen("CONNECTED");
-  doSend("WebSocket rocks");
+  // doSend("WebSocket rocks");
 }
 function onClose(evt){
   writeToScreen("DISCONNECTED");
 }
 function onMessage(evt){
   writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data +'</span>');
-  // проверка на наличие в массиве
-  for(let i=0; i<arr.length; i++){
-    if (evt.data == arr[i].index) {
-      arr.splice(i, 1)
+  for(let i=0; i < arr.length; i++){
+    if( evt.data == arr[i].id ){
+      remove(i)
+      console.log(0)
     }
-  };
-  list()
+    if( !evt.data == arr[i].id){
+      push_item();
+      console.log(1)
+      return false
+    }
+  console.log('evt.data:', evt.data, ";  arr[i].id:", arr[i].id)
+  }
 }
 function onError(evt){
   writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
@@ -34,7 +42,6 @@ function onError(evt){
 function doSend(message){
   writeToScreen("SENT: " + message);
   websocket.send(message);
-  list();
 }
 function writeToScreen(message){
   var pre = document.createElement("p");
@@ -45,6 +52,63 @@ function writeToScreen(message){
 
 window.addEventListener("load", init, false);
 
+
+function send_add(){
+  a++
+  writeToScreen("SENT: " + a );
+  websocket.send(a);
+}
+function send_remove(asd){
+  websocket.send(asd);
+}
+function remove(i){
+  arr.splice(i, 1);
+  list()
+}
+
+function push_item(){
+  let input = document.getElementById('input').value || 0;
+  let item = { text: input, id: a };
+  arr.push(item);
+  list()
+}
+function test(){
+  // console.log(arr)
+
+}
+
+
+
+// создание строки и вывод в ul
+function list(){
+  let ul = document.getElementById('ul');
+    ul.innerHTML = '';
+  for(let i=0; i < arr.length; i++ ){
+    // li
+    let li = document.createElement("li");
+      li.append(arr[i].text + '; ' + arr[i].id);
+    //  label
+    let div = document.createElement('div');
+      div.classList.add('item');
+    // button
+    let button = document.createElement('button');
+      button.classList.add('btn', 'btn-danger');
+      // button.setAttribute('onclick', 'doSend(' + arr[i].id + ')');
+      button.setAttribute('id', arr[i].id);
+      button.setAttribute('onclick', 'send_remove(this.getAttribute(\'id\'))');
+      button.append('remove');
+      div.append(button);
+      div.append(li);
+      ul.append(div);
+  }
+
+}
+
+
+
+
+
+
 // добавление события при нажатие на ENTER
 document.onkeyup = function (e) {
     e = e || window.event;
@@ -53,43 +117,3 @@ document.onkeyup = function (e) {
     }
     return false;
 }
-
-  let arr =[];
-  let a = 0;
-
-// добавление элемента в массив
-function send_to(){
-  let input = document.getElementById('input').value || 0;
-  let item = {text: input, index: a++}
-  document.getElementById('input').value = '';
-  arr.push(item);
-  list()
-}
-
-// создание строки и вывод в ul
-function list(){
-  let ul = document.getElementById('ul');
-    ul.innerHTML = '';
-  for(let i=0; i<arr.length; i++ ){
-    // li
-    let li = document.createElement("li");
-      li.append(arr[i].text + '; ' + arr[i].index);
-    //  label
-    let div = document.createElement('div');
-      div.setAttribute('id', arr[i].index);
-      div.classList.add('item');
-    // button
-    let button = document.createElement('button');
-      button.classList.add('btn', 'btn-danger');
-      button.setAttribute('onclick', 'doSend(' + arr[i].index + ')');
-      button.append('remove');
-      div.append(button);
-      div.append(li);
-      ul.append(div);
-  }
-}
-
-
-// function test(){
-//   console.log(arr)
-// }
